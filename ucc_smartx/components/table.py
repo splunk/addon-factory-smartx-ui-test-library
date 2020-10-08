2a74fee59900e6dc9b95446f4747546c45d2566b
seanmalloy-crest <smalloy@splunk.com>
2020-10-07 20:13:51 -0700

    Reverting some changes

--- ucc_smartx/components/table.py
+++ ucc_smartx/components/table.py
@@ -220,7 +220,6 @@ class Table(BaseComponent):
         _row.find_element(*list(self.elements["delete"]._asdict().values())).click()        
 
         self.wait_for("delete_prompt")
-        time.sleep(self.wait_for_seconds)
         if cancel:
             self.delete_cancel.click()
             self.wait_until("delete_cancel")
