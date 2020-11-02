9b848f87e74303f24876d986c52fb4076c778c76
jayjoshi-crest <jjoshi@splunk.com>
2020-11-02 22:37:52 +0530

    Fixed Alert Action singleselect component

--- pytest_splunk_addon_ui_smartx/alert_actions/components/account_select.py
+++ pytest_splunk_addon_ui_smartx/alert_actions/components/account_select.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from selenium.common.exceptions import TimeoutException
 from ...components.controls.single_select import SingleSelect
 from ...components.base_component import Selector, BaseComponent
 
@@ -16,6 +17,19 @@ class AlertAccountSelect(SingleSelect):
             "cancel_selected": Selector(select=container.select + ' button[data-icon-only="true"]')
         })
 
+    def wait_for_values(self):
+        """
+        Wait for dynamic values to load in SingleSelect
+        """
+        def _wait_for_values(driver):
+            try:
+                return self.get_single_value()
+            except Exception as e:
+                print("Wait_for_values failed with ::{}".format(str(e)))
+        try:
+            self.wait_for(_wait_for_values, msg="No values found in SingleSelect")
+        except TimeoutException:
+            return True
 
     def list_of_values(self):
         """
@@ -26,7 +40,20 @@ class AlertAccountSelect(SingleSelect):
         list_of_values = []
         for each in self.get_elements('values'):
             list_of_values.append(each.text.strip())
+        self.wait_to_be_clickable("dropdown")
         self.dropdown.click()
         self.wait_for("internal_container")
-        self.wait_to_be_clickable("dropdown")
         return list_of_values
+
+    def get_single_value(self):
+        """
+        Return one value from Single Select 
+        """
+        self.wait_to_be_clickable("dropdown")
+        self.dropdown.click()
+        single_element = self.get_element('values')
+
+        self.wait_to_be_clickable("dropdown")
+        self.dropdown.click()
+        self.wait_for("internal_container")
+        return single_element
