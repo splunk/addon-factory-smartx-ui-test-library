d496a3bc2c3b3a45b3ed7dcbe88fce1b22d9aea2
harshshah2-crest <61701682+harshshah2-crest@users.noreply.github.com>
2021-08-18 16:48:28 +0530

    test: updating selector for backwards compatibility

--- pytest_splunk_addon_ui_smartx/alert_actions/components/account_select.py
+++ pytest_splunk_addon_ui_smartx/alert_actions/components/account_select.py
@@ -16,7 +16,7 @@ class AlertAccountSelect(ActionControls):
             "dropdown": Selector(select=container.select + ' button[label="Select..."]'),
             "selected": Selector(select=container.select + ' button[type="button"] span[data-test="label"]'),
             "values": Selector(select='div[data-test="popover"] button[data-test="option"]'),
-            "cancel_selected": Selector(select=container.select + ' button[aria-label="Clear filter"]')
+            "cancel_selected": Selector(select=container.select + ' button[data-test="button"]')
         })
 
     def wait_for_values(self):
