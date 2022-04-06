564fc36b1967711bf28ce48221bfc548cdc55c0a
Artem Rys <79191415+arys-splunk@users.noreply.github.com>
2022-04-06 11:18:58 +0200

    chore: remove actionlint (#279)

--- pytest_splunk_addon_ui_smartx/alert_actions/components/table.py
+++ pytest_splunk_addon_ui_smartx/alert_actions/components/table.py
@@ -419,14 +419,10 @@ class AlertTable(ActionControls):
             :return: Bool whether or not switching to the page was successful
         """
         for each in self.get_elements("switch_to_page"):
-            if (
-                self.get_clear_text(each).lower()
-                not in [
-                    "prev",
-                    "next",
-                ]
-                and self.get_clear_text(each) == str(value)
-            ):
+            if self.get_clear_text(each).lower() not in [
+                "prev",
+                "next",
+            ] and self.get_clear_text(each) == str(value):
                 each.click()
                 return True
         else:
