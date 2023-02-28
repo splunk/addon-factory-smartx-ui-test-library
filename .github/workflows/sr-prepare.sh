eeaff727cce38eb39cc67d9a96c45c22fcda387a
mkolasinski-splunk <mkolasinski@splunk.com>
2023-02-28 12:47:32 +0100

    chore: include semantic-release-action and pypi-publish-action

--- .github/workflows/sr-prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eE
-set -v
-# shellcheck disable=SC1091,SC2086
-source $HOME/.poetry/env
-poetry build
--- .github/workflows/sr-release.sh
+++
@@ -1,7 +0,0 @@
-!/usr/bin/env bash
-
-set -eE
-set -v
-source $HOME/.poetry/env
-echo pypy user=${PYPI_USERNAME}
-poetry publish -n -u ${PYPI_USERNAME} -p ${PYPI_TOKEN}
