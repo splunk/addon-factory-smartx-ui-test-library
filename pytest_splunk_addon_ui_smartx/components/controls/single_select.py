0a13ff4626b982da97c232052300e4dbc5d5c167
mkolasinski-splunk <mkolasinski@splunk.com>
2023-10-24 16:55:26 +0200

    fix: singleselect is_editable works for listbox and combobox

--- pytest_splunk_addon_ui_smartx/components/controls/single_select.py
+++ pytest_splunk_addon_ui_smartx/components/controls/single_select.py
@@ -19,6 +19,7 @@ from time import sleep
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 
 from ..base_component import Selector
 from .base_control import BaseControl
@@ -51,6 +52,10 @@ class SingleSelect(BaseControl):
                 "dropdown": Selector(select=container.select + " .dropdownBox"),
                 "combobox": Selector(
                     select=container.select + ' [data-test="combo-box"]'
+                ),
+                "listbox_role": Selector(select=container.select + ' [role="listbox"]'),
+                "combobox_role": Selector(
+                    select=container.select + ' [role="combobox"]'
                 ),
                 "selected": Selector(
                     select=container.select + ' [data-test="textbox"]'
@@ -355,27 +360,17 @@ class SingleSelect(BaseControl):
             _wait_for_search_list, msg="No values found in SingleSelect search"
         )
 
-    def is_editable(self):
+    def is_editable(self) -> bool:
         """
-        Returns True if the Textbox is editable, False otherwise
+        Returns True if the SingleSelect is editable, False otherwise
         """
         if self.searchable:
-            if self.allow_new_values:
-                self.elements.update(
-                    {
-                        "input": Selector(
-                            select=self.container.select + ' [data-test="textbox"]'
-                        )
-                    }
+            element_key = "combobox_role" if self.allow_new_values else "listbox_role"
+            try:
+                self.get_element(element_key)
+            except TimeoutException as e:
+                raise Exception(
+                    f"{e}Make sure that SingleSelect option allow_new_values is properly configured within entity"
                 )
-            else:
-                self.dropdown.click()
-                popoverid = "#" + self.dropdown.get_attribute("data-test-popover-id")
-                self.elements.update(
-                    {"input": Selector(select=popoverid + ' [data-test="textbox"]')}
-                )
-        return not bool(
-            self.input.get_attribute("readonly")
-            or self.input.get_attribute("readOnly")
-            or self.input.get_attribute("disabled")
-        )
+            self.elements.update({"input": self.elements[element_key]})
+        return bool(self.input.get_attribute("type") == "text")
